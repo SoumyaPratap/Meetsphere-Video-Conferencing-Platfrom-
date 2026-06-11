be76e3e1a47972dd6e00f8607834283ffae23a19
SOUMYA PRATAP SINGH <soumyapratapsingh0001@gmail.com>
2026-06-11 09:02:18 +0530

    Fix CORS initialization

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const http = require("http");
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+const cors = require("cors");
 const { Server } = require("socket.io");
 
 const connectDB = require("./config/db");
@@ -20,7 +15,13 @@ const server = http.createServer(app);
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
